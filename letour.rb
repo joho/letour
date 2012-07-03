d769bc126e2596cb839ba692268c115142191caa
John Barton (joho) <jrbarton@gmail.com>
2012-07-03 11:41:47 +1000

    Bit more readability.

--- letour.rb
+++ letour.rb
@@ -13,6 +13,13 @@ videos_url = sbs_domain + "/cyclingcentral/videos"
 
 videos_page_doc = Nokogiri::HTML(open(videos_url))
 
-links = videos_page_doc.css("a").select { |l| l.attributes["title"] && !l.attributes["rel"] && l.attributes["title"].value =~ /tour de france stage \d+ extended highlights/i }.collect { |l| [l.attributes["title"].value, sbs_domain + l.attributes["href"].value]}
+highlights_title_regexp = /tour de france stage \d+ extended highlights/i
+
+links = videos_page_doc.css("a").select { |l| 
+    !l.attributes["rel"] && # ignore anything with a rel (it's for fancy javascript)
+    l.attributes["title"] && l.attributes["title"].value =~ highlights_title_regexp 
+  }.collect { |l| 
+    [l.attributes["title"].value, sbs_domain + l.attributes["href"].value]
+  }
 
 binding.pry
