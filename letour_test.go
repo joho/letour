b4f9fd519f132ed485e2a055f005894843f1e90a
John Barton (joho) <jrbarton@gmail.com>
2014-07-11 19:49:37 +1000

    Add a failing test with good/bad episode titles.

---
+++ letour_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestRegex(t *testing.T) {
+	goodTitles := []string{
+		"Tour de france stage 3 highlights",
+		"tour de france highlights stage 5",
+	}
+
+	for _, title := range goodTitles {
+		entry := Entry{Title: title}
+		if !entry.IsHighlight() {
+			t.Fatalf("didn't match good title: %v", title)
+		}
+	}
+
+	badTitles := []string{
+		"Tour De France 2014 Extended Highlights Stage 1",
+		"Tour De France 2014 Daily Highlights Ep1",
+	}
+	for _, title := range badTitles {
+		entry := Entry{Title: title}
+		if entry.IsHighlight() {
+			t.Fatalf("matched bad title: %v", entry.Title)
+		}
+	}
+}
